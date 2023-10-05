8929cb1f5bc7ab05436d4cedef52ffc3cfde7ebc
tokinorioe <irinkotironkot@gmail.com>
2023-10-05 18:56:55 +0900

    メッセージ機能の更新

--- SearchForLover/consumers.py
+++ SearchForLover/consumers.py
@@ -9,29 +9,29 @@ class RequestConsumer(AsyncWebsocketConsumer):
     
     #idとchannel名が辞書として保存された辞書をキューグループの代わりとして使う、スケーラビリティを考慮したイベントハンドリングをする
     
-    channel_id_mapping ={}
-    user_id = 39
+    channel_id_mapping: dict[int, str]
+    #channel名からuser_idを抽出する
+    user_id: int = 39
     
     
-    async def save_channel_with_id(self):
+    async def save_channel_with_id(self) -> None:
         self.channel_id_mapping[self.user_id] = self.channel_name
     
-    async def delete_channel_with_id(self):
+    async def delete_channel_with_id(self) -> None:
         del self.channel_id_mapping[self.user_id]
     
     #websocket接続を開始したら辞書にchannelを保存する
     #idが自身のidで、isRequestSentがFalseであるリクエストを探す、ヒットしたら全て送信する
-    async def connect(self): 
-        await self.accept() 
-        await self.save_channel_with_id()
-                
+    async def connect(self) -> None: 
+        self.accept() 
+        await self.save_channel_with_id() 
 
     #websocket接続を切断したらキューから対象channelを削除する
-    async def disconnect(self, close_code):
+    async def disconnect(self, close_code) -> None:
         await self.delete_channel_with_id()
     
     #websocket通信でrequestを送信
-    async def send_request(self, user_channel_name, sender, request_message):
+    async def send_request(self, user_channel_name, sender, request_message) -> None:
         try:
             self.channel_layer.send(
                 user_channel_name,
@@ -40,17 +40,18 @@ class RequestConsumer(AsyncWebsocketConsumer):
                     "request_message" : request_message
                 }
             )
-            '''
-            ここにreceiverからの受け取ったことを伝えるメールを確認するコードを書く
-            それを受け取って完了する->isRequestSentをtrueに変更する、メールが消失した場合、誤配信のエラーハンドリングを行う
-            '''
         except Exception as e:
             print(f"例外が発生しました:{e}")
-                
+            
     
-    async def receive(self, text_data):
-        #メッセージを受信し、DBに保存
-        # バリデーションをでシリアライズした後行う
+    #receiverからの受け取ったことを伝えるメールを確認するコードを書く
+    #それを受け取って完了する->isRequestSentをtrueに変更する、メールが消失した場合、誤配信のエラーハンドリングを行う
+    async def receive_confirmation_catchrequest(self, text) ->None:
+        pass
+                
+    #メッセージを受信し、DBに保存
+    # バリデーションをでシリアライズした後行う
+    async def receive(self, text_data) -> None:
         data = json.loads(text_data) 
         sender = data['sender']
         receiver = data['receiver']
