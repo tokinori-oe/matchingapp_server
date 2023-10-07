76c500f074cd4f41fd135a116623e1b90babcee4
tokinorioe <irinkotironkot@gmail.com>
2023-10-07 16:55:41 +0900

    モデルの変更

--- profiling/models.py
+++ profiling/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import  User
 class UserProfile(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length =20, unique= False, default = '')
+    nickname = models.CharField(max_length =20, unique= False)
     school_name = models.CharField(max_length=100)
     faculty = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
