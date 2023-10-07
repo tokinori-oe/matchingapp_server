fa590b9ced44abe878007cdae7fd1128c0306211
tokinorioe <irinkotironkot@gmail.com>
2023-10-07 17:25:46 +0900

    SearchForLoverのモデルの追加

--- SearchForLover/models.py
+++ SearchForLover/models.py
@@ -3,25 +3,19 @@ from profiling.models import UserProfile
 
 # Create your models here.
 class RequestForLoverModel(models.Model):
-    #受け取るユーザー
-    receiver = models.ForeignKey(UserProfile, related_name = 'received_requests', on_delete=models.CASCADE)
-    
-    #送るユーザー
-    sender = models.ForeignKey(UserProfile, related_name= 'sent_requests', on_delete=models.CASCADE)
-    
-    #メッセージ
+    '''
+    受け取るユーザーのid, nickname, 送るユーザーのid, nickname, メッセージ、リクエストが送られた日時、
+    リクエストの状態のmodel
+    '''
+    receiver = models.ForeignKey(UserProfile, related_name = 'receiver', on_delete=models.CASCADE)
+    sender = models.ForeignKey(UserProfile, related_name= 'sender', on_delete=models.CASCADE)
     request_message = models.TextField(blank = True)
-    
-    #リクエストの状態
     STATUS_CHOICES =(
         ('PENDING', '承認待ち'),
         ('APPROVED', '承認'),
         ('REJECTED', '拒否'),
     )
-    
     status = models.CharField(max_length=10, choices = STATUS_CHOICES, default='PENDING')
-    
-    #リクエストの作成日時
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -29,6 +23,15 @@ class RequestForLoverModel(models.Model):
         
     def __str__(self):
         return f"Request from {self.sender} to {self.receiver}: {self.status}"
-    
-    #def __str__(self):
-        #return self.user.username #ユーザー名を返すことでプロフィールを識別
+
+class UnsentRequestModel(models.Model):
+    '''
+    senderのid, receiverのid, senderのnickname, receiverのnickname, senderからのmessage、
+    senderがリクエストした日にちを入れる
+    '''
+    receiver = models.ForeignKey(UserProfile, related_name = 'receiver', on_delete=models.CASCADE)
+    receiver_nickname = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    sender = models.ForeignKey(UserProfile, related_name= 'sender', on_delete=models.CASCADE)
+    sender_nickname = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    request_message = models.TextField(blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
